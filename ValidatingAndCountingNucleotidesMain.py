7d86123c1122583e05043fa10bae223416b184cc
StitCJ <95364089+StitCJ@users.noreply.github.com>
2021-12-22 02:13:01 +0900

    Delete ValidatingAndCountingNucleotidesMain.py

--- ValidatingAndCountingNucleotidesMain.py
+++
@@ -1,9 +0,0 @@
-from rosalind.ValidatingAndCountingNucleotidesDNAToolkit import*
-import random
-
-# Creating a random DNA seqeunce for testing
-rndDNAstr = ''.join([random.choice(Nucleotides)
-                      for nuc in range(20)])
-
-print(validateSeq(rndDNAstr))
-print(countNucFrequency(rndDNAstr))
