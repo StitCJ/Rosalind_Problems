9915548906a0b3aacbe3cc9084e8dcc03638972f
StitCJ <95364089+StitCJ@users.noreply.github.com>
2021-12-23 00:17:09 +0900

    Add files via upload

---
+++ FIBO.py
@@ -0,0 +1,12 @@
+from functools import lru_cache
+
+@lru_cache
+def fib(n) :
+    if n == 0 :
+        return 0
+    elif n == 1 or n == 2 :
+        return 1
+    else :
+        return fib(n-1) + fib(n-2)
+    
+print(fib(25))
