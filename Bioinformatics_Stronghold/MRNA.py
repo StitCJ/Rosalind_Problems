0a568301226a3f7629dc30e608f35c4b82315e86
StitCJ <95364089+StitCJ@users.noreply.github.com>
2021-12-25 01:46:48 +0900

    Update MRNA.py

--- Bioinformatics_Stronghold/MRNA.py
+++ Bioinformatics_Stronghold/MRNA.py
@@ -1,3 +1,8 @@
+# Given: A protein string of length at most 1000 aa.
+# 
+# Return: The total number of different RNA strings from which the protein could have been translated, modulo 1,000,000. 
+# (Don't neglect the importance of the stop codon in protein translation.)
+
 aaDic = {
 'F' : 2, 'L' : 6, 'I' : 3, 'M' : 1, 'V' : 4, 'S' : 6, 'P' : 4, 'T' : 4, 'A' : 4, 'Y' : 2, 'H' : 2, 'Q' : 2, 'N' : 2, 'K' : 2, 'D' : 2, 'E' : 2, 'C' : 2, 'W' : 1, 'R' : 6, 'G' : 4
 }
